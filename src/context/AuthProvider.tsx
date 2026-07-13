d6fe2866b8ab2b95e53c888303e145e20317c40d
VasylMoskalyk <moskalykvasyl26@gmail.com>
2026-07-13 18:23:26 +0300

    fix token after refresh page

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,80 +1,78 @@
-import api from "@/lib/axios";
-import { useLayoutEffect, useState } from "react";
-import { AuthContext } from "./AuthContext";
+import api from '@/lib/axios';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
 
 type AuthProviderProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-export const AuthProvider = ({children}: AuthProviderProps) => {
-    const [token, setToken] = useState<string | null>(null);
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [token, setToken] = useState<string | null>(null);
 
-    // useEffect(()=>{
-    //     const fetchMe = async()=>{
-    //         try{
-    //             const response = await api.get('/auth/refresh-token');                
-    //             setToken(response.data.access_token);
-    //         }
-    //         catch {
-    //             setToken(null);
-    //         }
-    //     };
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        const response = await api.get('/auth/refresh-token');
+        setToken(response.data.accessToken);
+      } catch {
+        setToken(null);
+      }
+    };
 
-    //     fetchMe();
-    // }, [])
+    fetchMe();
+  }, []);
 
-    useLayoutEffect(()=>{
-        const authInterceptor = api.interceptors.request.use((config) => {
-            config.headers.Authorization = token ? `Bearer ${token}` : undefined;
-            return config;
-        });
+  useLayoutEffect(() => {
+    const authInterceptor = api.interceptors.request.use((config) => {
+      config.headers.Authorization = token ? `Bearer ${token}` : undefined;
+      return config;
+    });
 
-        console.log('useLayoutEffect ran auth interceptor')
+    return () => {
+      api.interceptors.request.eject(authInterceptor);
+    };
+  }, [token]);
 
-        return () => {
-            api.interceptors.request.eject(authInterceptor);
-        }
-    }, [token])
+  //     useLayoutEffect(() => {
+  //   const interceptor = api.interceptors.response.use(
+  //   (res) => res,
+  //   async (error) => {
+  //     const originalRequest = error.config;
 
-//     useLayoutEffect(() => {
-//   const interceptor = api.interceptors.response.use(
-//   (res) => res,
-//   async (error) => {
-//     const originalRequest = error.config;
+  //     if (
+  //       (error.response?.status === 401 || error.response?.status === 403) &&
+  //       !originalRequest._retry &&
+  //       originalRequest.url !== '/auth/refresh-token'
+  //     ) {
+  //       originalRequest._retry = true;
 
-//     if (
-//       (error.response?.status === 401 || error.response?.status === 403) &&
-//       !originalRequest._retry &&
-//       originalRequest.url !== '/auth/refresh-token'
-//     ) {
-//       originalRequest._retry = true;
+  //       try {
+  //         const response = await api.get('/auth/refresh-token');
 
-//       try {
-//         const response = await api.get('/auth/refresh-token');
+  //         setToken(response.data.access_token);
 
-//         setToken(response.data.access_token);
+  //         originalRequest.headers.Authorization =
+  //           `Bearer ${response.data.access_token}`;
 
-//         originalRequest.headers.Authorization =
-//           `Bearer ${response.data.access_token}`;
+  //         return api(originalRequest);
+  //       } catch (error) {
+  //       console.error(error)
+  //         setToken(null);
+  //       }
+  //     }
 
-//         return api(originalRequest);
-//       } catch (error) {
-//       console.error(error)
-//         setToken(null);
-//       }
-//     }
+  //     return Promise.reject(error);
+  //   }
+  // );
 
-//     return Promise.reject(error);
-//   }
-// );
+  //   return () => {
+  //     api.interceptors.response.eject(interceptor);
+  //   };
+  // }, []);
 
-//   return () => {
-//     api.interceptors.response.eject(interceptor);
-//   };
-// }, []);
-
-return (<AuthContext.Provider value={{ token, setToken }}>
-    {children}
-</AuthContext.Provider>)
-
-}
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
